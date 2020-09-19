9ec4c9f551c79928ef5519822a5d6a615f37db3c
Eric Vollenweider <eric.vollenweider.ch@gmail.com>
2020-09-19 14:01:32 +0200

    Increase update cycle of step counter

---
+++ Unity/HackZuerich/Assets/ProfileScripts.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ProfileScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LastPos = CamTransform.position;
+    }
+
+    public Transform CamTransform;
+
+    double totalDistance = 0;
+    Vector3 LastPos;
+
+
+    int counter = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter++ > 20){
+            double delta = Vector3.Magnitude(CamTransform.position - LastPos);
+            totalDistance += delta;
+            LastPos = CamTransform.position;
+            counter = 0;
+            Debug.Log($"Updated distance: {totalDistance}");
+            StepText.text = (1253 + (int)(totalDistance/0.7)).ToString();
+        }
+    }
+
+
+    public TextMeshPro StepText;
+}
