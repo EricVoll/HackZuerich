10ef2c5a43c9f00dc2cab79626683c6f8de8178a
Eric Vollenweider <eric.vollenweider.ch@gmail.com>
2020-09-19 15:01:46 +0200

    Config Items flying around

--- Unity/HackZuerich/Assets/UIHandler/WorkoutHandler.cs
+++ Unity/HackZuerich/Assets/UIHandler/WorkoutHandler.cs
@@ -7,8 +7,17 @@ public class WorkoutHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LaunchItems();
     }
     
+    public GameObject WorkoutItemPrefab;
+
+    private void LaunchItems(){
+        foreach(var ingredient in DataBase.instance.currentRecipe.data.ingredients){
+            var item = Instantiate(WorkoutItemPrefab);
+            item.GetComponent<WorkoutItem>().Setup(ingredient);
+            item.transform.parent = this.transform;
+        }
+    }
     
 }
--- Unity/HackZuerich/Assets/UIHandler/WorkoutItem.cs
+++ Unity/HackZuerich/Assets/UIHandler/WorkoutItem.cs
@@ -7,12 +7,49 @@ public class WorkoutItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public Material[] materials;
+
+    private float ScoreHealthToInt(string health){
+        switch(health){
+            case "A":
+                return .001f;
+            case "B":
+                return .0015f;
+            case "C":
+                return .002f;
+            case "D":
+                return .0025f;
+            case "E":
+                return .003f;
+            case "F":
+                return .0035f;
+        }
+        return 0.001f;
+    }
+
+    Dictionary<string, int> dict = new Dictionary<string, int>(){
+      { "A" , 0 },
+      { "B" , 1 },
+      { "C" , 2 },
+      { "D" , 3 },
+      { "E" , 4 },
+      { "F" , 5 },
+    };
+
+    public void Setup(Ingredient ingredient)
+    {
+        Vector3 speed = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f)).normalized;
+        speed = speed * ScoreHealthToInt(ingredient.ScoreHealth);
+        this.GetComponent<Rigidbody>().velocity = speed;
+        this.GetComponent<MeshRenderer>().material = materials[dict[ingredient.ScoreHealth]];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
