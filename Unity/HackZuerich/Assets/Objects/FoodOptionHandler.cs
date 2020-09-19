e0f0eba8804dc53d7ffbc2f60c705abe6c1f272a
Eric Vollenweider <eric.vollenweider.ch@gmail.com>
2020-09-19 02:52:59 +0200

    Basic Food Selection

---
+++ Unity/HackZuerich/Assets/Objects/FoodOptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FoodOptionHandler : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    public void SetFoodOption(FoodOption option)
+    {
+        if(meshRenderer ==  null){
+            Debug.LogError("MeshRenderer is null!");
+        }
+
+        meshRenderer.material = new Material(Shader.Find("Standard"));
+        var texture = option.GetTexture();
+        meshRenderer.material.SetTexture("_MainTex", texture);
+        Debug.Log("Set");
+    }
+
+    void Awake(){
+        SetFoodOption(new FoodOption());
+    }
+
+}
---
+++ Unity/HackZuerich/Assets/Objects/FoodSelectionMenuHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSelectionMenuHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject FoodOptionButtonPrefab;
+    public GameObject FoodOptionsContainer;
+    
+    void Awake(){
+        //Replace this with Rest Call later
+        Options = GetMockList();
+
+        foreach(var opt in Options){
+            GameObject btn = GameObject.Instantiate(FoodOptionButtonPrefab);
+            btn.transform.parent = FoodOptionsContainer.transform;
+            btn.GetComponent<FoodOptionHandler>().SetFoodOption(opt);
+        }
+
+        FoodOptionsContainer.GetComponent<Microsoft.MixedReality.Toolkit.Utilities.GridObjectCollection>().UpdateCollection();
+    }
+
+    public List<FoodOption> Options = new List<FoodOption>();
+
+    private List<FoodOption> GetMockList(){
+        List<FoodOption> options = new List<FoodOption>(){
+            new FoodOption(),
+            new FoodOption(),
+            new FoodOption(),
+            new FoodOption(),
+            new FoodOption(),
+            new FoodOption(),
+            new FoodOption(),
+            new FoodOption(),
+            new FoodOption(),
+        };
+        return options;
+    }
+}
